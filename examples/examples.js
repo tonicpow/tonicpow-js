8301af8c9dbc16cc30787d0a33a3d5f1c1b9beeb
mrz1836 <mrz1818@gmail.com>
2019-12-29 17:32:34 -0500

    Another refactor, using get/set pattern

--- examples/examples.js
+++ examples/examples.js
@@ -9,27 +9,27 @@ let apiKey = process.env.TONICPOW_API_KEY || ''
     //
     // Example: Load TonicPow api, creates a new session
     //
-    let response = await TonicPow.Load(apiKey,'local')
+    let response = await TonicPow.init(apiKey,'local')
     console.log(response)
 
     //
     // Example: Prolong a session (optional)
     //
-    response = await TonicPow.ProlongSession()
+    response = await TonicPow.prolongSession()
     console.log(response)
 
     //
     // Example: Login (as a user)
     //
-    response = await TonicPow.LoginUser("testing88577@tonicpow.com","ExamplePassForNow0!")
-    console.log(response, "user session token: ", TonicPow.UserSessionToken)
+    response = await TonicPow.loginUser("testing88577@tonicpow.com","ExamplePassForNow0!")
+    console.log(response, "user session token: ", TonicPow.session.userToken)
 
     //
     // Example: Current User (get user details)
     //
-    response = await TonicPow.CurrentUser(TonicPow.UserSessionToken)
+    response = await TonicPow.currentUser(TonicPow.session.userToken)
     console.log(response)
-    console.log("user session token: ", TonicPow.UserSessionToken)
+    console.log("user session token: ", TonicPow.session.userToken)
 
 
   } catch(e){
