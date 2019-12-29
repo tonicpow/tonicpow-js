9bb9badab42ff05c6ae1c561485eebed30c2cd13
mrz1836 <mrz1818@gmail.com>
2019-12-29 15:41:07 -0500

    More refactoring, cleanup

--- examples/examples.js
+++ examples/examples.js
@@ -9,25 +9,25 @@ let apiKey = process.env.TONICPOW_API_KEY || ''
     //
     // Example: Load TonicPow api, creates a new session
     //
-    let response = await TonicPow.load(apiKey)
+    let response = await TonicPow.Load(apiKey)
     console.log(response)
 
     //
     // Example: Prolong a session (optional)
     //
-    response = await TonicPow.prolong()
+    response = await TonicPow.ProlongSession()
     console.log(response)
 
     //
     // Example: Login (as a user)
     //
-    response = await TonicPow.loginUser("testing88577@tonicpow.com","ExamplePassForNow0!")
+    response = await TonicPow.LoginUser("testing88577@tonicpow.com","ExamplePassForNow0!")
     console.log(response, "user session token: ", TonicPow.UserSessionToken)
 
     //
     // Example: Current User (get user details)
     //
-    response = await TonicPow.currentUser(TonicPow.UserSessionToken)
+    response = await TonicPow.CurrentUser(TonicPow.UserSessionToken)
     console.log(response)
     console.log("user session token: ", TonicPow.UserSessionToken)
 
