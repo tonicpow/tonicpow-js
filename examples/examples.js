c7fee39c7fb0bc0fe5db0f165383b46afd157ccf
mrz1836 <mrz1818@gmail.com>
2019-12-29 12:32:45 -0500

    Got a few examples working

--- examples/examples.js
+++ examples/examples.js
@@ -1,21 +1,37 @@
-// Load the api wrapper
+// Load the api package
 let TonicPow = require('../lib/api')
 
-// Set the API key from our env
+// Set the API key from our local environment
 let apiKey = process.env.TONICPOW_API_KEY || ''
 
-// Load the TonicPow api
-TonicPow.load(apiKey).then(x => {
+;(async () => {
+  try {
+    //
+    // Example: Load TonicPow api, creates a new session
+    //
+    let response = await TonicPow.load(apiKey)
+    console.log(response)
 
-  console.log(x)
+    //
+    // Example: Prolong a session (optional)
+    //
+    response = await TonicPow.prolong()
+    console.log(response)
 
-  // Test prolong
-  TonicPow.prolong().then(x => {
-    console.log(x)
-  }).catch(e=>{
-    console.error("prolong error:",e)
-  });
+    //
+    // Example: Login (as a user)
+    //
+    response = await TonicPow.loginUser("testing88577@tonicpow.com","ExamplePassForNow0!")
+    console.log(response, "user session token: ", TonicPow.userSessionToken)
 
-}).catch(e=>{
-  console.error("load error:",e)
-});
+    //
+    // Example: Current User (get user details)
+    //
+    response = await TonicPow.currentUser(TonicPow.userSessionToken)
+    console.log(response, "user session token: ", TonicPow.userSessionToken)
+
+
+  } catch(e){
+    console.error(e)
+  }
+})();
