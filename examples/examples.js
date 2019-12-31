26973de15a123c23bd12bfd22bdf2cc561b01728
mrz1836 <mrz1818@gmail.com>
2019-12-31 15:02:29 -0500

    Minor changes to the examples

--- examples/examples.js
+++ examples/examples.js
@@ -6,6 +6,10 @@ let apiKey = process.env.TONICPOW_API_KEY || ''
 
 ;(async () => {
   try {
+
+    // Custom api session token (previous token) (cloud functions)
+    //let customApiToken = 'MTU3NzY2NTYyOXxTTGJtSEphWE15dC1DRUhRSzdrekJlYjVVVjM1bURPeTU4ckhsc2p2TjBnUnV0OGJ5NTNEVlhrdExzdmpPY2ZJRGZqMGVra0FjZE00S2NPTkk3WkgwZUc1MkNqanBiOTJ0Si1aN1BxTHE0Z3dYN0ZNLUtxT2VJYjlyRTZsdGd0UUI4NGdscVNSUEZQSnF4QlNxWi1LaUJzaEJkZGVMUkdhX09pN1NUVDQ5Y3hZeWlTV0haaTJDTmJuemUxVV9WU0N2bmNsUTkxNjNreUNOVmsyYTZlNVB3cGEtM3FFamlScHJNQTItZzA3YmpvdGtIQ19IcExIT3RCUEZyREp4SEIxSWZDRUhOUzhTOGxXQTdqRG83Qzh0LWZfRGwtTGJMM2pnUmtlUXZhVjJLWVBFeFVnb2hNNFlicnBqTkx2UnNkWEVnOVFWRWRjVUkxVjRPMXg3LUNaSXVoMHlySUlSWUZafLaGOta8msbLJKJmszMBw2CFTdE_92uG0UZuwQalP70Y'
+
     //
     // Example: Load TonicPow api, creates a new session
     //
@@ -13,11 +17,11 @@ let apiKey = process.env.TONICPOW_API_KEY || ''
     console.log(response)
 
     //
-    // Example: Load TonicPow api with existing session
+    // Example: Load TonicPow api with existing session token (cloud functions)
     //
-    //let customApiToken = 'MTU3NzY2NTYyOXxTTGJtSEphWE15dC1DRUhRSzdrekJlYjVVVjM1bURPeTU4ckhsc2p2TjBnUnV0OGJ5NTNEVlhrdExzdmpPY2ZJRGZqMGVra0FjZE00S2NPTkk3WkgwZUc1MkNqanBiOTJ0Si1aN1BxTHE0Z3dYN0ZNLUtxT2VJYjlyRTZsdGd0UUI4NGdscVNSUEZQSnF4QlNxWi1LaUJzaEJkZGVMUkdhX09pN1NUVDQ5Y3hZeWlTV0haaTJDTmJuemUxVV9WU0N2bmNsUTkxNjNreUNOVmsyYTZlNVB3cGEtM3FFamlScHJNQTItZzA3YmpvdGtIQ19IcExIT3RCUEZyREp4SEIxSWZDRUhOUzhTOGxXQTdqRG83Qzh0LWZfRGwtTGJMM2pnUmtlUXZhVjJLWVBFeFVnb2hNNFlicnBqTkx2UnNkWEVnOVFWRWRjVUkxVjRPMXg3LUNaSXVoMHlySUlSWUZafLaGOta8msbLJKJmszMBw2CFTdE_92uG0UZuwQalP70Y'
-    //let response = await TonicPow.init(apiKey, 'local', customApiToken)
+    //let response = await TonicPow.init(apiKey, TonicPow.config.environments.Local, customApiToken)
     //console.log(response)
+    //console.log(TonicPow.session.apiToken)
 
     //
     // Example: Prolong a session (optional)
@@ -41,9 +45,12 @@ let apiKey = process.env.TONICPOW_API_KEY || ''
     // Example: Login (as a user)
     //
     response = await TonicPow.loginUser('testing88577@tonicpow.com','ExamplePassForNow0!')
-    console.log(response, 'user session token: ', TonicPow.session.userToken)
+    console.log(response)
+    //console.log('user session token: ', TonicPow.session.userToken)
 
-    // Example setting a user token from a cookie header (used for Cloud functions)
+    //
+    // Example: Setting a user token from a cookie header (used for Cloud functions)
+    //
     TonicPow.session.userToken = `Cookie: session_token=`+TonicPow.session.userToken+`; another_cookie=value; third_cookie=value`
 
     //
